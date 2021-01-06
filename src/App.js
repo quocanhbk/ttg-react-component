7544c1d28583adbb83981f43e256250533061669
Quan <sieutihons10@users.noreply.github.com>
2021-01-06 11:36:15 +0700

    Add TableDatePicker

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import {ThemeProvider} from 'styled-components'
 import Container from './components/Container'
 import theme from './utils/theme'
 import {Checkbox, CheckBoxGroup, Radio, RadioGroup, Button, ButtonGroup} from './components/elements'
+import TableDatePicker from "./components/TableDatePicker"
+
+const data = [
+  {id: 1, name:"banana"},
+  {id: 2, name:"Orange"},
+  {id: 3,name:"Mango"}
+]
 
 function App() {
   return (
     <div style={{display: "flex"}}>
       <ThemeProvider theme={theme.light}>
         <Container title="Light">
-          <ButtonGroup demo>
+        <ButtonGroup demo>
             <Button value={1}>One</Button>
             <Button value={2}>Two</Button>
             <Button value={3}>Three</Button>
           </ButtonGroup>
-          <Checkbox value={1} onSelect={(value) => console.log(value)}>One</Checkbox>
+          <TableDatePicker>Don't forget</TableDatePicker>
         </Container>
       </ThemeProvider>
     </div>
---
+++ src/components/TableDatePicker.js
@@ -0,0 +1,51 @@
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
+import {useState} from "react"
+import styled from 'styled-components'
+
+
+const StyleTable = styled.div`
+    position:relative;
+    & .react-datepicker__day-name{
+        color: ${props => props.theme.backgroundColor};
+    }
+    & .react-datepicker__current-month{
+        color: ${props => props.theme.backgroundColor};
+    }
+    & .react-datepicker__day--selected , .react-datepicker__day--keyboard-selected{
+        background: ${props => props.theme.fillColor};
+        color:  ${props => props.theme.backgroundColor};       
+    }
+    & .react-datepicker-popper{
+        transform:none!important;
+    }
+    & .react-datepicker__tab-loop{
+        position:relative;
+    }
+    & .react-datepicker__header{
+        background:${props => props.theme.fillColor};
+    }
+    & .react-datepicker__close-icon::after{
+        background:${props => props.theme.fillColor};
+    }
+`;
+
+
+const  TableDatePicker = (props) => {
+    const [startDate, setStartDate] = useState(new Date());
+   
+    return (
+        <StyleTable {...props}>
+      <DatePicker selected={startDate}
+       dateFormat="dd/MM/yyyy"
+        onChange={date => setStartDate(date)}
+        isClearable
+        placeholderText="Select date"
+        >
+      <div style={{ color: "red" }}>{props.children}</div>
+    </DatePicker>
+      </StyleTable>
+    );
+   }
+
+export default TableDatePicker
