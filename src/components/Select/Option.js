75882bd07b5c403968f4d4d9c31d9c468b1f2916
Quốc Đạt <65354747+Quoc-Dat1205@users.noreply.github.com>
2021-01-08 16:43:11 +0700

    input

---
+++ src/components/Select/Option.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const OneOption = styled.option`
+
+`;
+const Option = (props)=>{
+    return(
+        <OneOption>
+            One
+        </OneOption>
+    )
+}
+export default Option
---
+++ src/components/Select/Select.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const DivParent = styled.select`
+   
+`;;
+const Select = (props)=>{
+    return(
+        <DivParent>
+            Click
+        </DivParent>
+    )
+}
+export default Select
