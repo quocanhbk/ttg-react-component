f44871bbd4fc7f8dfb8c0746d22b726115f739d2
Quốc Đạt <65354747+Quoc-Dat1205@users.noreply.github.com>
2021-01-12 13:28:49 +0700

    combo box

--- src/Dat.js
+++ src/Dat.js
@@ -1,18 +1,14 @@
 import React from 'react'
-import ComboBox from './components/Dropdown/ComboBox'
 import AccordionComponent from './components/Accordioncomponent'
 import Badge from './components/Badge/Badge'
-import Search from './components/Search/Search'
+import ComboBox from './components/Search/ComboBox'
 
 const data = ['banana','mango','apple','fruits']
 export default function Dat() {
     return (
         <>
-          <Badge
-            value="3"
-            />
           {/* <ComboBox value={data}/> */}
-          {/* <Search/> */}
+          <ComboBox value={data}/>
         </>
     )
 }
