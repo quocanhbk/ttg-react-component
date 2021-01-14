3d63c6caaa9d85c53f8e0a6109bf8b77da3115b8
Quốc Đạt <65354747+Quoc-Dat1205@users.noreply.github.com>
2021-01-14 14:55:50 +0700

    update tablebody

--- src/components/Table/TableBody.js
+++ src/components/Table/TableBody.js
@@ -1,11 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useSortableData from './useSortableData'
 import IcoSort from '../icons/IcoRepeat'
 const TableBody = (props) => {
     const { headers, rows } = props;
     const {requestSort} = useSortableData(rows)
-
-
+    const [rotate, setRotate] = useState(false);
     const buildRow = (rows, headers) => {
       return (
            <tr key={rows.id}>
@@ -15,10 +14,18 @@ const TableBody = (props) => {
            </tr>
        )
     };
+    const handleRotate = ()=>{
+      if(rotate === true){
+        setRotate(false)
+      }
+      else{
+        setRotate(true)
+      }
+    }
     return(
         <tbody>
           <tr className="tr-sort-data">
-            {headers.map((value,index) => <th key={index}><button className="button-sort" onClick={() => requestSort(value)}><IcoSort/></button></th>)}
+            {headers.map((value,index) => <th key={index}><button className="button-sort" onClick={() => requestSort(value)}><IcoSort rotate={rotate} onClick={()=>handleRotate()}/></button></th>)}
           </tr>
           { rows && rows.map((value) => {
                   return buildRow(value, headers);
